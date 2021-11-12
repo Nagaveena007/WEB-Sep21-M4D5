952ae7e1f83013b69ba80a48855303692ff5ef46
Bilal Ahmad <bilal.ahmad0796@gmail.com>
2021-11-12 13:24:59 +0100

    navbar completed

--- netflix/src/App.js
+++ netflix/src/App.js
@@ -1,9 +1,10 @@
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import Navbar from './Components/MyNavbar'
 function App() {
   return (
-    <Navbar/>
+    <Navbar />
   );
 }
 
--- netflix/src/Components/MyNavbar.jsx
+++ netflix/src/Components/MyNavbar.jsx
@@ -1,28 +1,32 @@
-import logo from 'netflix/src/Components/MyNavbar.jsx'
+import image from '../Data/Netflix_Logo_RGB.png'
+import avatar from '../Data/avatar.png'
+import lens from '../Data/lens.svg'
+import bell from '../Data/bell-solid.svg'
 import { Navbar, Nav, NavDropdown } from "react-bootstrap"
 const MyNavbar = () => (
-    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-        <Navbar.Brand href="#home">
-            logo
-        </Navbar.Brand>
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse id="responsive-navbar-nav">
-            <Nav className="mr-auto">
-                <Nav.Link href="#features">Features</Nav.Link>
-                <Nav.Link href="#pricing">Pricing</Nav.Link>
-                <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
-                    <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                    <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                    <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                    <NavDropdown.Divider />
-                    <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-                </NavDropdown>
+    <Navbar collapseOnSelect expand="lg" style={{backgroundColor:'black'}}>
+        <img src={image} width='100px' height='40px' alt="" />
+        <Navbar.Toggle aria-controls="responsive-navbar-nav"/>
+        <Navbar.Collapse id="responsive-navbar-nav" >
+            <Nav className="mr-auto"  >
+                <Nav.Link href="#features" style={{color:'white'}} >Home</Nav.Link>
+                <Nav.Link href="#pricing" style={{color:'white'}}>TV Shows</Nav.Link>
+                <Nav.Link href="#features" style={{color:'white'}}>Movies</Nav.Link>
+                <Nav.Link href="#pricing" style={{color:'white'}}>Recently Added</Nav.Link>
+                <Nav.Link href="#pricing" style={{color:'white'}}>MyList</Nav.Link>
             </Nav>
             <Nav>
-                <Nav.Link href="#deets">More deets</Nav.Link>
-                <Nav.Link eventKey={2} href="#memes">
-                    Dank memes
-                </Nav.Link>
+                <div className='container mr-1'>
+                <img src={lens} width='30px' height='20px' alt="" />
+                <Nav.Link href="#features" style={{color:'white', marginRight:'5px'}} >KIDS</Nav.Link>
+                <img src={bell} width='30px' height='20px' alt="" />
+                <img src={avatar} width='40px' height='40px' style={{borderRadius:'30%',marginLeft:'20px'}} alt="" />
+                <NavDropdown id="dropdown-item-button">
+                    <NavDropdown.Item href="#action/3.1">Profile</NavDropdown.Item>
+                    <NavDropdown.Item href="#action/3.2">Settings</NavDropdown.Item>
+                    <NavDropdown.Item href="#action/3.3">Log Out</NavDropdown.Item>
+                </NavDropdown>
+                </div>
             </Nav>
         </Navbar.Collapse>
     </Navbar>
