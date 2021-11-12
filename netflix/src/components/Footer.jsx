8a0901d0d4d588d5b3748cf73cc6f1a7de1f1ba2
Michael Redruello <redruellomichael@gmail.com>
2021-11-12 16:22:58 +0100

    Footer completed
    It needs to be fixed at the bottom of the page

---
+++ netflix/src/components/Footer.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+
+const Footer = (props) => (
+    <div className="main-footer">
+        <div className="foot-div1">
+            <ul className ="foot-ul1">
+                <li className ="content-li"><a className="links" href="#">Audio and Subtititles</a></li>
+                <li className ="content-li"><a  className="links" href="#">Media Center</a></li>
+                <li className ="content-li"><a className="links"  href="#">Privacy</a></li>
+                <li className ="content-li"><a  className="links"  href="#">Contact Us</a></li>
+                <li className ="content-li"><a  className="links"  href="#">Service Code</a></li>
+                <li className ="content-li"><a  className="links"  href="#">© 1997-2019 Netflix, inc.</a></li>
+            </ul>
+        </div>
+        <div className="foot-div2">
+            <ul className ="foot-ul2">
+                <li className ="content-li"><a  className="links" href="#">Audio Description</a></li>
+                <li className ="content-li"><a   className="links" href="#">Investor Relations</a></li>
+                <li className ="content-li"><a  className="links" href="#">Legal Notices</a></li>
+            </ul>
+        </div>
+        <div className="foot-div3">
+            <ul className ="foot-ul3">
+                <li className ="content-li"><a  className="links" href="#">Help Center</a></li>
+                <li className ="content-li"><a  className="links" href="#">Jobs</a></li>
+                <li className ="content-li"><a  className="links" href="#">Cookie Preferences</a></li>
+            </ul>
+        </div>
+        <div className="foot-div4">
+            <ul className ="foot-ul4">
+                <li className ="content-li"><a  className="links" href="#">Gift Cards</a></li>
+                <li className ="content-li"><a  className="links" href="#">Terms of Use</a></li>
+                <li className ="content-li"><a  className="links" href="#">Corporate Information</a></li>
+            </ul>
+        </div>
+    </div>
+)
+
+export default Footer
